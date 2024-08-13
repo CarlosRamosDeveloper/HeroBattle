a8e9328e9be099a674497f0f9955fdd317411e93
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-08-13 07:02:47 +0200

    Hero created

---
+++ src/Hero.java
@@ -0,0 +1,111 @@
+public class Hero {
+    String name;
+    int minDamage;
+    int maxDamage;
+    int maxHealthPoints;
+    int actualHealthPoints;
+    int dodgeRating;
+    int criticalRate;
+    boolean isAlive;
+    boolean hasDodged;
+    boolean hasDeliveredCritical;
+    boolean isStunned;
+
+    public Hero(String name, int maxDamage, int maxHealthPoints, int dodgeRating, int criticalRate) {
+        this.name = name;
+        this.minDamage = 1;
+        this.maxDamage = maxDamage;
+        this.maxHealthPoints = maxHealthPoints;
+        this.actualHealthPoints = maxHealthPoints;
+        this.dodgeRating = dodgeRating;
+        this.criticalRate = criticalRate;
+        this.isAlive = true;
+        this.hasDodged = false;
+        this.hasDeliveredCritical = false;
+        this.isStunned = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMinDamage() {
+        return minDamage;
+    }
+
+    public void setMinDamage(int minDamage) {
+        this.minDamage = minDamage;
+    }
+
+    public int getMaxDamage() {
+        return maxDamage;
+    }
+
+    public void setMaxDamage(int maxDamage) {
+        this.maxDamage = maxDamage;
+    }
+
+    public int getMaxHealthPoints() {
+        return maxHealthPoints;
+    }
+
+    public void setMaxHealthPoints(int maxHealthPoints) {
+        this.maxHealthPoints = maxHealthPoints;
+    }
+
+    public int getActualHealthPoints() {
+        return actualHealthPoints;
+    }
+
+    public void setActualHealthPoints(int actualHealthPoints) {
+        this.actualHealthPoints = actualHealthPoints;
+    }
+
+    public int getDodgeRating() {
+        return dodgeRating;
+    }
+
+    public void setDodgeRating(int dodgeRating) {
+        this.dodgeRating = dodgeRating;
+    }
+
+    public int getCriticalRate() {
+        return criticalRate;
+    }
+
+    public void setCriticalRate(int criticalRate) {
+        this.criticalRate = criticalRate;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void kill() {
+        this.isAlive = false;
+    }
+
+    public boolean isHasDodged() {
+        return hasDodged;
+    }
+
+    public void setHasDodged(boolean hasDodged) {
+        this.hasDodged = hasDodged;
+    }
+
+    public boolean isHasDeliveredCritical() {
+        return hasDeliveredCritical;
+    }
+
+    public void setHasDeliveredCritical(boolean hasDeliveredCritical) {
+        this.hasDeliveredCritical = hasDeliveredCritical;
+    }
+
+    public boolean isStunned() {
+        return isStunned;
+    }
+
+    public void setStunned(boolean stunned) {
+        isStunned = stunned;
+    }
+}
